344fed25bf5b12c200a9d670171d082b4c52c8c4
Austin Pocus <ajpocus@gmail.com>
2021-09-14 16:52:35 -0400

    well, shit

--- main.py
+++ main.py
@@ -1,60 +1,54 @@
+import threading
+import time
 import pyaudio
-import wave
+
 from scipy import signal
-from scipy.io import wavfile
-import matplotlib
-import matplotlib.pyplot as plt
 
-# CHUNK = 1024
-# FORMAT = pyaudio.paInt16
-# CHANNELS = 2
-# RATE = 44100
-# RECORD_SECONDS = 5
-# WAVE_OUTPUT_FILENAME = "output.wav"
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 15
+WAVE_OUTPUT_FILENAME = "output.wav"
+BPM = 120
+NOTE = 1/8
 
-# p = pyaudio.PyAudio()
+pa = pyaudio.PyAudio()
 
-# stream = p.open(format=FORMAT,
-#                 channels=CHANNELS,
-#                 rate=RATE,
-#                 input=True,
-#                 frames_per_buffer=CHUNK)
+stream = pa.open(format=FORMAT,
+				channels=CHANNELS,
+				rate=RATE,
+				input=True,
+				frames_per_buffer=CHUNK)
 
-# print("* recording")
+print("* recording")
 
-# frames = []
+beat_seconds = 60 / BPM
+interval = beat_seconds * NOTE
 
-# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-#     data = stream.read(CHUNK)
-#     frames.append(data)
+def record():
+	threading.Timer(interval, record).start()
 
-# print("* done recording")
+	frames = []
+	t_end = time.time() + 60 / BPM * pow(NOTE, 2)
+	while time.time() < t_end:
+		data = list(stream.read(CHUNK))
+		frames.append(data)
 
-# stream.stop_stream()
-# stream.close()
-# p.terminate()
+	# Compute PSD:
+	f, P = signal.periodogram(frames, fs=RATE) # Frequencies and PSD
 
-# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-# wf.setnchannels(CHANNELS)
-# wf.setsampwidth(p.get_sample_size(FORMAT))
-# wf.setframerate(RATE)
-# wf.writeframes(b''.join(frames))
-# wf.close()
+	max_index = P.argmax()
+	power = P[max_index]
+	print("POWER", power)
+	if power > 50:
+		freq = f[max_index]
+		print(freq)
 
-# Read the file (rate and data):
-rate, data = wavfile.read('data/1k.wav') # See source
+record()
 
-# Compute PSD:
-f, P = signal.periodogram(data, rate) # Frequencies and PSD
+print("* done recording")
 
-# Display PSD:
-fig, axe = plt.subplots()
-axe.semilogy(f, P)
-axe.set_xlim([0,2e3])
-axe.set_ylim([1e-8, 1e10])
-axe.set_xlabel(r'Frequency, $\nu$ $[\mathrm{Hz}]$')
-axe.set_ylabel(r'PSD, $P$ $[\mathrm{AU^2Hz}^{-1}]$')
-axe.set_title('Periodogram')
-axe.grid(which='both')
-axe.plot()
-plt.savefig("test.jpg")
+stream.stop_stream()
+stream.close()
+pa.terminate()
