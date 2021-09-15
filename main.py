30f1df8764560932f25b32b9127f42cb08c3a4e5
Austin Pocus <ajpocus@gmail.com>
2021-09-15 09:18:53 -0400

    getting the name of the most powerful note

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import wave
 import sys
+
+from math import log2, pow
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 from scipy import signal
 
 CHANNELS = 1
@@ -11,6 +15,16 @@ BEAT_SECONDS = 60 / BPM
 CHUNK = 1024
 NOTE = 1/8
 FREQ_LIMIT = 3
+A4 = 440
+C0 = A4 * pow(2, -4.75)
+NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+def pitch(freq):
+    h = round(12 * log2(freq / C0))
+    octave = h // 12
+    n = h % 12
+
+    return NAMES[n] + str(octave)
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
@@ -42,17 +56,15 @@ def main():
 		powers = P	# get the non-mirrored data from the frequency analysis
 
 		largest_powers = list(reversed(np.argsort(powers)))
-		indices = largest_powers[:FREQ_LIMIT]
-
-		for idx in indices:
-			power = powers[idx]
-			print("POWER", power)
-			freq = f[idx]
-			print(freq)
+		idx = largest_powers[0]
+		power = powers[idx]
+		print("POWER", power)
+		freq = f[idx]
+		print("FREQ", freq)
+		print("PITCH", pitch(freq))
 
 	f, P = get_signal(data)
 
-	print("RMS", np.sqrt(P.max()))
 	plt.semilogy(f, P)
 	plt.show()
 	print("* done")
